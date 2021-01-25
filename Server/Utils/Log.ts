799d24191036e170aa81f1b1857cb47e9d8ef002
Maxime Leriche <maxleriche.60@gmail.com>
2021-01-25 23:31:00 +0100

    [Add] Store et log lib

---
+++ Server/Utils/Log.ts
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+module.exports ={
+    /**
+     * * Permet de console.log 
+     * @param tag Qui envoie le message
+     * @param message Message envoyer par l'utilisateur
+     */
+    Log: async (tag:string,message:string)=>{
+        console.log(chalk`{bold [${tag}]}=> ${new Date().toISOString()} : ${message}`)
+    },
+    /**
+     * * Permet de log une erreur
+     * @param tag Qui envoie le message
+     * @param message Message envoyer par l'utilisateur
+     */
+    ErrorLog: async (tag:string,message:string)=>{
+        console.log(chalk`{red.bold [${tag}]}=> ${new Date().toISOString()} : ${message}`)
+    }
+}
---
+++ Server/Utils/Store.ts
@@ -0,0 +1,30 @@
+const path = require('path')
+const fs = require('fs')
+const {Log} = require('./Log')
+
+class Store{
+    nconf
+    soundBoardRoot
+
+    constructor(){
+        this.nconf = require('nconf')
+        this.soundBoardRoot = path.join(process.env.APPDATA,"SoundBoard")
+        this.nconf
+            .argv()
+            .env()
+        if(this.CreateFolderIfNotExist(this.soundBoardRoot)){
+            this.CreateFolderIfNotExist(path.join(this.soundBoardRoot,"Store"))
+            this.AddFileConfig(path.join(this.soundBoardRoot,"Store","config.json"))
+            Log("Store","Le store est importer")
+        }
+
+    }
+    CreateFolderIfNotExist = (pathDir:string)=>!fs.existsSync(pathDir)?fs.mkdirSync(pathDir):true
+    AddFileConfig = (pathDir:string) => this.nconf.file({file:pathDir})
+    GetConf = () => this.nconf
+    GetVar = (store:string,variable:string) => this.nconf.get(`${store}:${variable}`)
+    SaveConf = (callback:Function)=> this.nconf.save(callback)
+
+}
+
+module.exports.Store
