2c10bd0ab91946fc07bc866b29bcfa9723c41dea
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-14 20:50:59 +0100

    [add] junk thing

--- Server/index.ts
+++ Server/index.ts
@@ -1,10 +1,17 @@
+console.log("Serveur Start")
 require('dotenv-flow').config()
 import WebServer from './Utils/Server'
-
+import Store from './Utils/Store'
+import DataBase from './Utils/Db'
+var store = new Store();
 var Serveur = new WebServer(Number(process.env.SERVER_PORT))
+var Db = new DataBase(store);
 
 console.log(process.env.SERVER_PORT)
 console.log("JE suis un test2")
+
+console.log(store.GetConf("db"))
+
 
 
 
