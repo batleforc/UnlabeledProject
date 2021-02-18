1c75e1e3ce9d39196858491b7175513986501c7b
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-18 01:14:51 +0100

    [add] ajout de login et disconnect Discord Instance

--- Server/Utils/Discord.ts
+++ Server/Utils/Discord.ts
@@ -2,8 +2,12 @@ import Discordjs from 'discord.js'
 
 class Discord{
   client : Discordjs.Client
-  constructor(Token : string) {
+  constructor() {
     this.client = new Discordjs.Client();
   }
   GetClient = ()=>this.client
+  LoginClient = (token : string) => this.client.login(token)
+  DisconnectClient = () => this.client.destroy()
 }
+
+export default Discord;
