8f343e762e624171c3fffc738159df78f1ae4055
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-18 23:02:44 +0100

    [add] ajout de set presence + prise en compte des undefined

--- Server/Utils/Discord.ts
+++ Server/Utils/Discord.ts
@@ -1,18 +1,28 @@
-import Discordjs from 'discord.js'
+import Discordjs, { ActivityType, Guild, Presence, PresenceData } from 'discord.js'
 
 class Discord{
   client : Discordjs.Client
   constructor() {
     this.client = new Discordjs.Client();
   }
-  GetClient = ()=>this.client
+  GetClient = () => this.client
+
   LoginClient = (token : string) => this.client.login(token)
   DisconnectClient = () => this.client.destroy()
 
   GetAllServer = () => this.client.guilds.cache
-  GetOneServer = (GuildId : string) => this.client.guilds.cache.get(GuildId)
-  GetAllChan = (GuildId : string) => this.GetOneServer(GuildId).channels.cache
-  GetOneChan = (GuildId : string , ChanId : string) => this.GetAllChan(GuildId).get(ChanId)
+  GetOneServer = ( guildId : string ) => this.client.guilds.cache.get(guildId)
+  GetAllChan = ( guildId : string ) => this.GetOneServer(guildId)?.channels.cache
+  GetOneChan = ( guildId : string , ChanId : string ) => this.GetAllChan(guildId)?.get(ChanId)
+
+  SetPresenceFromPresenceData = (presence : PresenceData ) => this.client.user?.setPresence(presence)
+  SetPresence = (online : boolean , name : string , type : ActivityType) => this.SetPresenceFromPresenceData({
+    status : online ? "online" : "idle" ,
+    activity : {
+      name : name ,
+      type : type
+    }
+  })
 }
 
 export default Discord;
