6caaf67e3e65bb0deadb65dfe12beedfa694cfd7
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-18 01:21:42 +0100

    [add] GetAllServer GetOneServer GetAllChan

--- Server/Utils/Discord.ts
+++ Server/Utils/Discord.ts
@@ -8,6 +8,10 @@ class Discord{
   GetClient = ()=>this.client
   LoginClient = (token : string) => this.client.login(token)
   DisconnectClient = () => this.client.destroy()
+
+  GetAllServer = () => this.client.guilds.cache
+  GetOneServer = (GuildId : string) => this.client.guilds.cache.get(GuildId)
+  GetAllChan = (GuildId : string) => this.GetOneServer(GuildId).channels.cache
 }
 
 export default Discord;
