de5c4bacc2e87d5facc539bef26d20dc8332e46c
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-23 08:57:07 +0100

    [add] mise en place de 4 couleur tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors:{
+      primary   : "#7F1D1D",
+      secondary : "#18181B",
+      white     : "#F3F4F6",
+      grey      : "#71717A"
+    }
   },
   variants: {
     extend: {},
