a81c1bdf0d3b1959fbc1faef4b5d6c733708f53b
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-25 17:59:27 +0100

    [add] V1 de la navbar

--- src/component/navbar/index.tsx
+++ src/component/navbar/index.tsx
@@ -3,16 +3,19 @@ import { connect } from 'react-redux'
 
 export const index = ({Bot,...props}: any) => {
   return (
-    <nav className="bg-primary flex">
-      <div id="Label" className="flex-1">
-        <h1>{process.env.REACT_APP_NAME}</h1>
+    <nav className="bg-secondary text-white flex">
+      <div id="Label" className=" px-2 justify-start ">
+        <p className="text-lg">{process.env.REACT_APP_NAME}</p>
       </div>
-      <div id="Nav" className="flex-1">
+      <div id="Nav" className="flex-1 justify-start">
         
       </div>
-      <div id="BotLogo" className="flex-1 place-items-center text-right">
-        <img className=" z-10 w-9 rounded float-right" src={Bot.img} />
-        <p>{Bot.user?Bot.user.tag:""}</p>
+      <div id="BotLogo" className="flex flex-1 place-items-center text-center justify-end leading-3">
+        <div className="px-2">
+          <p className="text-base">{Bot.user?Bot.user.tag:""}</p>
+          <a className="text-xs" href={Bot.link} target="_blank" rel="noreferrer">Lien d'invitation</a>
+        </div>
+        <img className=" z-10 w-9 rounded" src={Bot.img} />
       </div>
     </nav>
   )
