a67efd0e2ee8f9a00f03c6c6690502d1154723e0
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-25 23:08:40 +0100

    [add] Form Création Token

--- src/component/Token/TokenForm.tsx
+++ src/component/Token/TokenForm.tsx
@@ -1,14 +1,27 @@
 import React,{useState} from 'react'
 import { connect } from 'react-redux'
+import {TokenCreate} from '../../Action/Token'
 
 export const TokenForm = ({dispatch,...props}:any) => {
   var [label,setLabel] = useState("");
   var [token,setToken] = useState("");
+  var send = () =>{
+    dispatch(TokenCreate({label,token}))
+  }
   return (
-    <div>
-      <input type="text" name="Label" value={label} onChange={event=>setLabel(event.target.value)} />
-      <input type="text" name="Token" value={token} onChange={event=>setToken(event.target.value)} />
-      <input type="button" name="send" value="Send" />
+    <div className=" px-3 py-3 space-y-4 justify-self-center">
+      <p>Create Token : </p>
+      <div className="flex flex-col flex-wrap content-center">
+        <label className="block">
+          <span>Label</span>
+          <input className="px-1 rounded-md border border-primary block" placeholder="Label" type="text" name="Label" value={label} onChange={event=>setLabel(event.target.value)} />
+        </label>
+        <label className="block">
+          <span>Token</span>
+          <input className="px-1 rounded-md border border-primary block" placeholder="Token" type="text" name="Token" value={token} onChange={event=>setToken(event.target.value)} />
+        </label>
+        <input className="flex rounded-md border border-primary px-2" type="button" name="send" value="Send" onClick={send} />
+      </div>
     </div>
   )
 }
