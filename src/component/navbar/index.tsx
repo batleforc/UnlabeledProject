f914a941cf08e76ddbbc9e099a018027de279566
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-25 22:28:38 +0100

    [add] add link to / and /token

--- src/component/navbar/index.tsx
+++ src/component/navbar/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export const index = ({Bot,...props}: any) => {
   return (
     <nav className="bg-secondary text-white flex">
       <div id="Label" className=" px-2 justify-start ">
-        <p className="text-lg">{process.env.REACT_APP_NAME}</p>
+        <Link to="/">
+          <p className="text-lg">{process.env.REACT_APP_NAME}</p>
+        </Link>
       </div>
       <div id="Nav" className="flex-1 justify-start">
         
@@ -17,9 +20,9 @@ export const index = ({Bot,...props}: any) => {
           <a className="text-xs" href={Bot.link} target="_blank" rel="noreferrer">Lien d'invitation</a>
         </div>}
         {Bot.link===""&&
-        <div className="px-2">
+        <Link to="/token" className="px-2">
           <p className="text-base">No bot Ready</p>
-        </div>}
+        </Link>}
         <img className=" z-10 w-9 rounded" src={Bot.img} />
       </div>
     </nav>
