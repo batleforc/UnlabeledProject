6e19645b5b97951d00e2d8a0676df3ca0c15f70d
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-25 18:38:52 +0100

    [add] Filtre de ce qui est retourner sur la récupération des serveur

--- Server/Router/Api/Bot.ts
+++ Server/Router/Api/Bot.ts
@@ -4,7 +4,16 @@ var Bot = new Router();
 Bot
   .get("/serveur", async (ctx : any , next : any) => {
     if(ctx.discord.Ready)
-      ctx.body=ctx.body = ctx.discord.GetAllServer()
+      ctx.body=ctx.body = ctx.discord.GetAllServer().map((value:any,index:number)=>{
+        return {
+          id:value.id,
+          ServeurName :value.name,
+          ServeurAcronyme:value.nameAcronym,
+          Icon : value.iconURL(),
+          nbrMembre:value.memberCount,
+          ownerID:value.ownerID
+        }
+      })
     else
       ctx.body={
         message:"Bot not started yet or not ready"
