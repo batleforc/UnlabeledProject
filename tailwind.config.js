ebfde57adbccb0be80db9651afa781e06edfe324
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-27 14:43:05 +0100

    [add] Keep default theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 const colors = require('tailwindcss/colors')
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors:{
+      ...defaultColors,
       primary   : "#7F1D1D",
       secondary : "#18181B",
       white     : "#F3F4F6",
       grey      : "#71717A"
-    }
+    },
+    fill: theme => theme('colors')
   },
   variants: {
     extend: {},
