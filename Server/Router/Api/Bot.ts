e4a31a61787eb5a3481dfb7173d454d4e49588ce
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-27 13:34:53 +0100

    [add] Stop bot

--- Server/Router/Api/Bot.ts
+++ Server/Router/Api/Bot.ts
@@ -24,10 +24,15 @@ Bot
   })
   .post("/start", async (ctx : any, next :any) => {
     if(ctx.request.body.id===undefined)
-    if(!ctx.discord.Ready)
+    if(ctx.discord.Ready)
       (ctx.discord as Discord).DisconnectClient();
-    (ctx.discord as Discord).LoginClient((ctx.Db as DataBase).GetToken(ctx.request.body.id).token);
+    (ctx.discord).LoginClient((ctx.Db as DataBase).GetToken(ctx.request.body.id).token);
     ctx.body={message:"all Is Green"}
+    next()
+  })
+  .post("/stop", async (ctx : any, next :any) => {
+    (ctx.discord as Discord).DisconnectClient();
+    ctx.body={message:"all Is Green "}
     next()
   })
 
