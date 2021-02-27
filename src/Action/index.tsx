dfbddb6adcdf238e7b37947d83c11043e3d359c1
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-27 19:52:45 +0100

    [add] Ah j'ai oublier de commit cette partie....

--- src/Action/index.tsx
+++ src/Action/index.tsx
@@ -1,11 +1,13 @@
 import { configureStore} from '@reduxjs/toolkit'
 import Token from './Token'
+import ShowModal from './showModal'
 import Bot from './Bot'
 
 export default configureStore({
   reducer: {
     Token,
-    Bot
+    Bot,
+    ShowModal
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools:true
