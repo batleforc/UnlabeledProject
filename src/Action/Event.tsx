c0a9a822d313bd2b9ddbca329b50bcfebd6af619
Maxime Leriche <maxleriche.60@gmail.com>
2021-02-28 17:04:02 +0100

    [add] Add action

--- src/Action/Event.tsx
+++ src/Action/Event.tsx
@@ -1,16 +1,45 @@
-import {createSlice} from '@reduxjs/toolkit'
-
+import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
 interface Event {
-  ReloadBot : Boolean
+  ReloadBot : Boolean,
+  Ready : Boolean,
+  Pending : Boolean
 }
+const initialState = {
+  ReloadBot: false,
+  Ready:false,
+  Pending:false
+} as Event
 
-const initialState = {ReloadBot: false} as Event
+export const EventInit = createAsyncThunk(
+  "Event/init",
+  async ({socket}:any,{dispatch}) =>{
+
+  },{
+    condition:(force:boolean|void,{getState}) : boolean => {
+      var test = getState()
+      if((test as any).Event.Ready)
+        return false
+      return true
+    }
+  }
+)
+
+
 
 const EventSlicer = createSlice({
   name:"Event",
   initialState,
   reducers:{
 
+  },
+  extraReducers : builder => {
+    builder
+      .addCase(EventInit.pending,(state)=>{state.Pending=true})
+      .addCase(EventInit.rejected,(state)=>{state.Pending=false})
+      .addCase(EventInit.fulfilled,(state)=>{
+        state.Pending=false
+        state.Ready=true
+      })
   }
 })
 
