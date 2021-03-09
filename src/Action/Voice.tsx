caad18306d03f5ad13645e0d754aaf62cc8f19ee
Maxime Leriche <maxleriche.60@gmail.com>
2021-03-10 00:47:19 +0100

    [add] create function voice handler

--- src/Action/Voice.tsx
+++ src/Action/Voice.tsx
@@ -1,4 +1,48 @@
 import { createSlice , createAsyncThunk} from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const joinChan = createAsyncThunk(
+  "voice/Join",
+  async ({guildId,ChanId}: any,{dispatch}) => {
+
+  }
+)
+export const leaveChan = createAsyncThunk(
+  "voice/leaveChan",
+  async ({guildId,ChanId}: any,{dispatch}) => {
+
+  }
+)
+export const getVolume = createAsyncThunk(
+  "voice/getVolume",
+  async ({guildId,ChanId}: any,{dispatch}) => {
+
+  }
+)
+export const stopVoice = createAsyncThunk(
+  "voice/stopVoice",
+  async ({guildId,ChanId}: any,{dispatch}) => {
+
+  }
+)
+export const startVoice = createAsyncThunk(
+  "voice/startVoice",
+  async ({guildId,ChanId}: any,{dispatch}) => {
+
+  }
+)
+export const resumeVoice = createAsyncThunk(
+  "voice/resumeVoice",
+  async ({guildId,ChanId}: any,{dispatch}) => {
+
+  }
+)
+export const setVolume = createAsyncThunk(
+  "voice/setVolume",
+  async ({guildId,ChanId}: any,{dispatch}) => {
+
+  }
+)
 
 interface Voice{
   Pending   : boolean,
