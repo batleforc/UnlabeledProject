b249b5e556584f15dc91a995714dd6384d176ed1
Maxime Leriche <maxleriche.60@gmail.com>
2021-03-10 01:48:59 +0100

    [add] handle case when no server available

--- src/component/navbar/ServeurSelect.tsx
+++ src/component/navbar/ServeurSelect.tsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { BotServeurChanGetter } from '../../Action/Bot'
 
 export const ServeurSelect = ({Bot,dispatch}:any) => {
+  if(Bot.Serveur.message!==undefined)
+    return <div></div>
   return (
     <div id="Nav" className={`flex-1 flex justify-center place-items-center ${Bot.user!==undefined?"":"hidden"}`}>
       {Bot.ActiveServeur!=="-1"&&<img alt="Serveur logo" className="z-10 h-9 w-9 rounded" src={Bot.Serveur.find((value:any)=>value.id===Bot.ActiveServeur)?.Icon} />}
