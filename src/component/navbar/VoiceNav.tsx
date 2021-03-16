94918f68f137692f7759714bdf699fa507d4d9a9
Maxime Leriche <maxleriche.60@gmail.com>
2021-03-17 00:11:07 +0100

    [add] Volume grey car ne fonctione pas encore

--- src/component/navbar/VoiceNav.tsx
+++ src/component/navbar/VoiceNav.tsx
@@ -35,7 +35,7 @@ export const VoiceNav = ({Voice,Bot,dispatch} : any) => {
         <svg xmlns="http://www.w3.org/2000/svg" onClick={()=>dispatch(stopVoice())} className="bg-gray-400 rounded-md hover:bg-gray-200 mx-0.5" style={{width:"24px",height:"24px"}} fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-        <div>
+        <div className="hover:bg-gray-900">
           <label htmlFor="volume">Volume:</label>
           <input className="bg-grey" type="number"name="volume" min="0" max="10" onChange={(event)=>dispatch(setVolume(Number(event.target.value)/10))} value={Voice.Volume} />
         </div>
