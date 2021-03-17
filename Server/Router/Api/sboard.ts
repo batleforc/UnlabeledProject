b6bf198f534b06c6a26e3ba11ed85c69765cfa8a
Maxime Leriche <maxleriche.60@gmail.com>
2021-03-17 23:01:50 +0100

    [add] Setup Delete Tab et tabitem

--- Server/Router/Api/sboard.ts
+++ Server/Router/Api/sboard.ts
@@ -15,6 +15,24 @@ sBoard
     ctx.body=ctx.Db.GetAllTabItem(ctx.params.id)
     next()
   })
+  .delete("/tab/:ItemId", async (ctx : any, next : any) =>{
+    if(ctx.params.ItemId===undefined){
+      ctx.status = 400
+      ctx.body={message:"Error one argument missing"}
+      return next()
+    }
+    ctx.body=ctx.Db.DeleteTabItem(ctx.params.ItemId)
+    next()
+  })
+  .delete("/:TabId", async (ctx : any, next : any) =>{
+    if(ctx.params.TabId===undefined){
+      ctx.status = 400
+      ctx.body={message:"Error one argument missing"}
+      return next()
+    }
+    ctx.body=ctx.Db.DeleteTab(ctx.params.TabId)
+    next()
+  })
 
 
 
