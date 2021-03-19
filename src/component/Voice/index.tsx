d7a0269989b73a9f6d05b7453eafb01e02f23378
Maxime Leriche <maxleriche.60@gmail.com>
2021-03-19 02:14:33 +0100

    [add] TabPanel

--- src/component/Voice/index.tsx
+++ src/component/Voice/index.tsx
@@ -2,8 +2,9 @@ import React,{useState,useEffect} from 'react';
 import { connect } from 'react-redux';
 import {startVoice} from '../../Action/Voice'
 import GridLayout, { Layout } from 'react-grid-layout'
+import { GetBoard, setActiveBoard } from '../../Action/sBoard';
 
-export const Index = ({Voice,Bot,dispatch} : any) => {
+export const Index = ({sBoard,Voice,Bot,dispatch} : any) => {
   var enumMedia ={1:"mp3",2:"Youtube",3:"spotify"}
   var [sound,setSound] = useState("")
   var [button,setButton] = useState("")
@@ -14,7 +15,8 @@ export const Index = ({Voice,Bot,dispatch} : any) => {
     {i: "3", x: 4, y: 0, w: 1, h: 2,text:"baby shark",url:"https://www.myinstants.com/media/sounds/baby-shark-dance-sing-and-dance-animal-songs-pinkfong-songs-for-children-audiotrimmer.mp3",type:1,static:true},
     {i: "4", x: 4, y: 0, w: 1, h: 2,text:"moan",url:"https://www.myinstants.com/media/sounds/moaning-sex.mp3",type:1,static:true}
   ])
-  useEffect(()=>console.log(layout),[layout])
+  // eslint-disable-next-line
+  useEffect(()=>dispatch(GetBoard()),[])
   var addButton = ()=>{
     setLayout(layout.concat([ {i:String(layout.length+1), x: 0, y: 0, w: 1, h: 2,text:button,url:url,type:1,static:layout[0].static}]))
     setButton("")
@@ -39,6 +41,17 @@ export const Index = ({Voice,Bot,dispatch} : any) => {
         <button onClick={()=>addButton()} > addButton </button>
         <button className="mx-2" onClick={lock} > Lock </button>
         <button onClick={unLock} > UnLock </button>
+      </div>
+      <div>
+        <nav className="flex flex-col sm:flex-row">
+          {sBoard.Board.map((value:any)=>
+          <button
+            className={`text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none ${sBoard.ActiveBoard===value.id?"border-b-2 font-medium border-blue-500":""}`}
+            onClick={(event)=>dispatch(setActiveBoard(Number(event.currentTarget.value)))}
+            key={value.id} value={value.id}>
+            {value.label}
+          </button>)}
+        </nav>
       </div>
       <GridLayout onLayoutChange={setHandlerLayout} className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
         {layout.map(value=><div key={value.i} className="bg-red-500 border-black border-2 cursor-pointer select-none" onClick={()=>{if(value.static)dispatch(startVoice(value.url))}} >{value.text}</div>)}
