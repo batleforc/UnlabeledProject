a863e89d76041d8de7c56b7aea2346a2a01d456f
Maxime Leriche <maxleriche.60@gmail.com>
2021-03-20 18:41:37 +0100

    [add] Mise en place du clic on outside close modal
    Je vais devoir penser a vendre mon component Modal

--- src/component/Modal/index.tsx
+++ src/component/Modal/index.tsx
@@ -1,24 +1,36 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useRef} from 'react'
 
 export const Modal = ({on,warn,exitOnEscape=true,title=process.env.REACT_APP_NAME,Content,message,activateText,activate,deactivateText,deactivate}:any) => {
+  const modalRef = useRef<HTMLDivElement>(null);
   const onEscape = ({key}:any)=>{
     if(key==="Escape"){
       activate()
       window.removeEventListener('keydown',onEscape);
     }
   }
+  const onOutside = (event : any) =>{
+    console.log("plop")
+    if(!modalRef.current?.contains(event.target)){
+      activate()
+    }
+  }
   useEffect(()=>{
     if(exitOnEscape){
       window.addEventListener("keydown",onEscape);
-    return () => window.removeEventListener('keydown',onEscape);
+      window.addEventListener("mousedown",onOutside)
+    return () => {
+      window.removeEventListener('keydown',onEscape);
+      window.removeEventListener("mousedown",onOutside)
     }
-  })
+    }
+    // eslint-disable-next-line
+  },[])
   return(
   <div className={`fixed z-10 inset-0 overflow-y-auto ${on?"":"hidden"}`}>
     <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
       <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
       <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-        <div className="bg-grey px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div ref={modalRef} className="bg-grey px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
           <div className="sm:flex sm:items-start">
             {warn&&
               <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-200 sm:mx-0 sm:h-10 sm:w-10">
