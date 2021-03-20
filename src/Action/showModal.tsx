4023490d37b9d792726ad7d672f4cd4d586e635b
Maxime Leriche <maxleriche.60@gmail.com>
2021-03-20 17:59:41 +0100

    [add] start CreateTab et createButton Modal

--- src/Action/showModal.tsx
+++ src/Action/showModal.tsx
@@ -1,22 +1,36 @@
 import {createSlice} from '@reduxjs/toolkit'
 
 interface Modal{
-  TokenForm:Boolean
+  TokenForm     : Boolean,
+  CreateTab     : Boolean,
+  CreateButton  : Boolean
 }
 
-const initialState ={TokenForm:false} as Modal
+const initialState ={
+  TokenForm     : false,
+  CreateTab     : false,
+  CreateButton  : false
+} as Modal
 
 const ShowModalSlicer = createSlice({
   name:"showModal",
   initialState,
   reducers :{
     showTokenForm : (state) => {state.TokenForm=true},
-    hideTokenForm : (state) => {state.TokenForm=false}
+    hideTokenForm : (state) => {state.TokenForm=false},
+    showCreateTab : (state) => {state.CreateTab=true},
+    hideCreateTab : (state) => {state.CreateTab=false},
+    showCreateButton : (state) => {state.CreateButton=true},
+    hideCreateButton : (state) => {state.CreateButton=false},
   }
 })
 
 export const {
   showTokenForm,
-  hideTokenForm
+  hideTokenForm,
+  showCreateTab,
+  hideCreateTab,
+  showCreateButton,
+  hideCreateButton
 } = ShowModalSlicer.actions
 export default ShowModalSlicer.reducer
