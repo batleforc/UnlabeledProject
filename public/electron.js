d9edbcfbc8cc42f78c7adcb6723ac9da5518e4da
Maxime Leriche <maxleriche.60@gmail.com>
2021-03-22 21:36:21 +0100

    [add] node-abi+conf electron.js

--- public/electron.js
+++ public/electron.js
@@ -3,17 +3,24 @@ const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
 const url = require('url');
-
+const Server = require('../BuildServer/index')
 let mainWindow;
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    autoHideMenuBar:true
+  });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : "http://localhost:5000/" ); //`file://${path.join(__dirname, '../build/index.html')}`
   mainWindow.on('closed', () => mainWindow = null);
   mainWindow.once('ready-to-show', () => mainWindow.show());
 }
 
-app.on('ready', createWindow);
+app.on('ready', ()=>{
+  Server()
+  createWindow()
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
