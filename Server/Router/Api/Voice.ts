e2fc9453a855ac9e87daeee9b4365df8694dcddb
Maxime Leriche <maxleriche.60@gmail.com>
2021-03-27 11:32:31 +0100

    [lint] retrait de return inutile

--- Server/Router/Api/Voice.ts
+++ Server/Router/Api/Voice.ts
@@ -5,64 +5,66 @@ var Voice = new Router();
 Voice
   .get("/volume", async (ctx : any, next : any)=>{
     ctx.body=(ctx.discord as Discord).VoiceGetVolume()||5
-    return await next()
+    await next()
   })
   .get("/pause",async (ctx : any, next : any)=>{
     ctx.body=(ctx.discord as Discord).VoiceGetIsPaused()
-    return await next()
+    await next()
   })
   .get("/status",async (ctx : any, next : any)=>{
     ctx.body=(ctx.discord as Discord).VoiceGetStatus()
-    return await next()
+    await next()
   })
   .get("/",async (ctx : any, next : any)=>{
     ctx.body=(ctx.discord as Discord).VoiceGetChan()
-    return await next()
+    await next()
   })
   .post("/join",async (ctx : any, next : any)=>{
     var {guildId,chanId} = ctx.request.body
     if(guildId===undefined||chanId===undefined){
       ctx.body={message:"Param manquant",guildId:guildId===undefined,chanId:chanId===undefined}
+    }else{
+      (ctx.discord as Discord).VoiceJoin(guildId,chanId,ctx.io)
+      ctx.body={
+        launched:true
+      };
     }
-    (ctx.discord as Discord).VoiceJoin(guildId,chanId,ctx.io)
-    ctx.body={
-      launched:true
-    };
-    return await next()
+    await next()
   })
   .post("/leave",async (ctx : any, next : any)=>{
     (ctx.discord as Discord).VoiceLeave()
     ctx.body={launched:true}
-    return await next()
+    await next()
   })
   .post("/volume",async (ctx : any, next : any)=>{
     var {vol} = ctx.request.body
     if(vol===undefined){
       ctx.body={message:"Param manquant",vol:vol===undefined}
-      return await next()
+    }else{
+      (ctx.discord as Discord).VoiceVolume(vol);
+      ctx.body={launched:true}
     }
-    (ctx.discord as Discord).VoiceVolume(vol);
-    ctx.body={launched:true}
-    return await next()
+    await next()
   })
   .post("/play",async (ctx : any, next : any)=>{
     var {toPlay,option} = ctx.request.body
     if(toPlay===undefined){
       ctx.body={message:"Param manquant ",toPlay:toPlay===undefined}
+    }else{
+      (ctx.discord as Discord).VoicePlay(toPlay,option)
+      ctx.body={launched:true}
     }
-    (ctx.discord as Discord).VoicePlay(toPlay,option)
-    ctx.body={launched:true}
-    return await next()
+    await next()
   })
   .post("/pause",async (ctx : any, next : any)=>{
     ctx.body=(ctx.discord as Discord).VoiceStop()
     ctx.body={launched:true}
-    return await next()
+    await next()
   })
   .post("/resume",async (ctx : any, next : any)=>{
     ctx.body=(ctx.discord as Discord).VoiceResume()
     ctx.body={launched:true}
-    return await next()
+    await next()
   })
 
 
