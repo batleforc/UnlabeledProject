e1430cb87048aa025ad4a740afb85a1ec4d704c9
gh-actions <actions@github.com>
2021-04-01 16:11:06 +0000

    Deploy website - based on df40cb85b5813ecc10027f5df77b60cb764c0ebb

--- assets/js/a027c50c.925033d8.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{92:function(l){l.exports=JSON.parse('{"allTagsPath":"/UnlabeledProject/blog/tags","slug":"hello","name":"hello","count":1,"permalink":"/UnlabeledProject/blog/tags/hello"}')}}]);
---
+++ assets/js/d4e634df.aa2c88d0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{92:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Bienvenue","permalink":"/UnlabeledProject/blog/Bienvenue"}]}')}}]);
