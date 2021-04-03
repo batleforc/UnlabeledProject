1fad1f57ec6e01f53cd513e1a58dca800fd48ebb
Maxime Leriche <maxleriche.60@gmail.com>
2021-04-03 23:07:12 +0200

    [lint+add] lint voiceApi et voiceHandler + add delete at

--- Server/Router/Api/Voice.ts
+++ Server/Router/Api/Voice.ts
@@ -1,83 +1,99 @@
-import Router from '@koa/router'
-import Discord from '../../Utils/Discord';
-import {Song} from '../../Utils/VoiceHandler'
+import Router from "@koa/router";
+import Discord from "../../Utils/Discord";
+import { Song } from "../../Utils/VoiceHandler";
 var Voice = new Router();
 
-Voice
-  .get("/volume", async (ctx : any, next : any)=>{
-    ctx.body=(ctx.discord as Discord).getVoice().getVolume()
-    await next()
+Voice.get("/volume", async (ctx: any, next: any) => {
+  ctx.body = (ctx.discord as Discord).getVoice().getVolume();
+  await next();
+})
+  .get("/pause", async (ctx: any, next: any) => {
+    ctx.body = (ctx.discord as Discord).getVoice().getIsPaused();
+    await next();
   })
-  .get("/pause",async (ctx : any, next : any)=>{
-    ctx.body=(ctx.discord as Discord).getVoice().getIsPaused()
-    await next()
+  .get("/status", async (ctx: any, next: any) => {
+    ctx.body = (ctx.discord as Discord).getVoice().getStatus();
+    await next();
   })
-  .get("/status",async (ctx : any, next : any)=>{
-    ctx.body=(ctx.discord as Discord).getVoice().getStatus()
-    await next()
+  .get("/", async (ctx: any, next: any) => {
+    ctx.body = (ctx.discord as Discord).getVoice().getVoiceStatus();
+    await next();
   })
-  .get("/",async (ctx : any, next : any)=>{
-    ctx.body=(ctx.discord as Discord).getVoice().getVoiceStatus()
-    await next()
-  })
-  .post("/join",async (ctx : any, next : any)=>{
-    var {guildId,chanId} = ctx.request.body
-    if(guildId===undefined||chanId===undefined){
-      ctx.body={message:"Param manquant",guildId:guildId===undefined,chanId:chanId===undefined}
-    }else{
-      (ctx.discord as Discord).VoiceJoin(guildId,chanId,ctx.io)
-      ctx.body={
-        launched:true
+  .post("/join", async (ctx: any, next: any) => {
+    var { guildId, chanId } = ctx.request.body;
+    if (guildId === undefined || chanId === undefined) {
+      ctx.body = {
+        message: "Param manquant",
+        guildId: guildId === undefined,
+        chanId: chanId === undefined,
+      };
+    } else {
+      (ctx.discord as Discord).VoiceJoin(guildId, chanId, ctx.io);
+      ctx.body = {
+        launched: true,
       };
     }
-    await next()
+    await next();
   })
-  .post("/leave",async (ctx : any, next : any)=>{
-    (ctx.discord as Discord).VoiceLeave(ctx.io)
-    ctx.body={launched:true}
-    await next()
-  })
-  .post("/volume",async (ctx : any, next : any)=>{
-    var {vol} = ctx.request.body
-    if(vol===undefined){
-      ctx.body={message:"Param manquant",vol:vol===undefined}
-    }else{
-      (ctx.discord as Discord).getVoice().SetVolume(ctx.io,vol)
-      ctx.body={launched:true}
+  .post("/skipAt", async (ctx: any, next: any) => {
+    var {id} = ctx.request.body
+    if (id === undefined) {
+      ctx.body = {
+        message: "Param manquant",
+        SongId: id === undefined,
+      };
+    } else {
+      (ctx.discord as Discord).getVoice().DeleteSong(ctx.io,id)
+      ctx.body = {
+        launched: true,
+      };
     }
-    await next()
+    await next();
   })
-  .post("/play",async (ctx : any, next : any)=>{
-    var {toPlay,now} = ctx.request.body
-    if(toPlay===undefined){
-      ctx.body={message:"Param manquant ",toPlay:toPlay===undefined}
-    }else{
-      (ctx.discord as Discord).getVoice().Play(ctx.io,(toPlay as Song),now)
-      ctx.body={launched:true}
+  .post("/leave", async (ctx: any, next: any) => {
+    (ctx.discord as Discord).VoiceLeave(ctx.io);
+    ctx.body = { launched: true };
+    await next();
+  })
+  .post("/volume", async (ctx: any, next: any) => {
+    var { vol } = ctx.request.body;
+    if (vol === undefined) {
+      ctx.body = { message: "Param manquant", vol: vol === undefined };
+    } else {
+      (ctx.discord as Discord).getVoice().SetVolume(ctx.io, vol);
+      ctx.body = { launched: true };
     }
-    await next()
+    await next();
   })
-  .post("/pause",async (ctx : any, next : any)=>{
-    ctx.body=(ctx.discord as Discord).getVoice().Pause(ctx.io)
-    ctx.body={launched:true}
-    await next()
+  .post("/play", async (ctx: any, next: any) => {
+    var { toPlay, now } = ctx.request.body;
+    if (toPlay === undefined) {
+      ctx.body = { message: "Param manquant ", toPlay: toPlay === undefined };
+    } else {
+      (ctx.discord as Discord).getVoice().Play(ctx.io, toPlay as Song, now);
+      ctx.body = { launched: true };
+    }
+    await next();
   })
-  .post("/resume",async (ctx : any, next : any)=>{
-    ctx.body=(ctx.discord as Discord).getVoice().Resume(ctx.io)
-    ctx.body={launched:true}
-    await next()
+  .post("/pause", async (ctx: any, next: any) => {
+    ctx.body = (ctx.discord as Discord).getVoice().Pause(ctx.io);
+    ctx.body = { launched: true };
+    await next();
   })
-  .post("/stop",async (ctx : any, next : any)=>{
-    ctx.body=(ctx.discord as Discord).getVoice().Stop(ctx.io)
-    ctx.body={launched:true}
-    await next()
+  .post("/resume", async (ctx: any, next: any) => {
+    ctx.body = (ctx.discord as Discord).getVoice().Resume(ctx.io);
+    ctx.body = { launched: true };
+    await next();
   })
-  .post("/skip",async (ctx : any, next : any)=>{
-    ctx.body=(ctx.discord as Discord).getVoice().Skip(ctx.io)
-    ctx.body={launched:true}
-    await next()
+  .post("/stop", async (ctx: any, next: any) => {
+    ctx.body = (ctx.discord as Discord).getVoice().Stop(ctx.io);
+    ctx.body = { launched: true };
+    await next();
   })
-
-
+  .post("/skip", async (ctx: any, next: any) => {
+    ctx.body = (ctx.discord as Discord).getVoice().Skip(ctx.io);
+    ctx.body = { launched: true };
+    await next();
+  });
 
 export default Voice;
--- Server/Utils/VoiceHandler.ts
+++ Server/Utils/VoiceHandler.ts
@@ -1,159 +1,156 @@
-import { VoiceChannel, VoiceConnection } from 'discord.js'
-import {canJoin} from './Permissions'
-import { Server } from 'socket.io';
-import {ErrorLog} from './Log'
-import ytdl from 'ytdl-core'
-export enum SongType{
+import { VoiceChannel, VoiceConnection } from "discord.js";
+import { canJoin } from "./Permissions";
+import { Server } from "socket.io";
+import { ErrorLog } from "./Log";
+import ytdl from "ytdl-core";
+export enum SongType {
   link = 1,
   YouTube = 2,
-  Spotify = 3
+  Spotify = 3,
 }
 
-export interface Song{
-  title : string
-  url   : string,
-  type  : SongType
+export interface Song {
+  title: string;
+  url: string;
+  type: SongType;
 }
 
-export interface SongQueue{
-  voiceChannel  : VoiceChannel | null
-  connection    : VoiceConnection | null
-  volume        : number
-  playing       : boolean
-  canPlay       : boolean
-  queue         : Array<Song>
+export interface SongQueue {
+  voiceChannel: VoiceChannel | null;
+  connection: VoiceConnection | null;
+  volume: number;
+  playing: boolean;
+  canPlay: boolean;
+  queue: Array<Song>;
 }
 
-class VoiceHandler{
-  songQueue : SongQueue
+class VoiceHandler {
+  songQueue: SongQueue;
 
-  constructor(){
+  constructor() {
     this.songQueue = {
-      voiceChannel : null,
-      playing      : false,
-      volume       : 5,
-      queue        : [],
-      connection   : null,
-      canPlay      : false
-    } as SongQueue
+      voiceChannel: null,
+      playing: false,
+      volume: 5,
+      queue: [],
+      connection: null,
+      canPlay: false,
+    } as SongQueue;
   }
 
-  getQueue = () => this.songQueue.queue
-  getVolume = () => this.songQueue.volume
-  getServer = () => this.songQueue.voiceChannel?.guild
-  getChannel = () => this.songQueue.voiceChannel
-  getIsPaused = () => this.songQueue.connection?.dispatcher?.paused
-  getStatus = () => this.songQueue.connection?.status
+  getQueue = () => this.songQueue.queue;
+  getVolume = () => this.songQueue.volume;
+  getServer = () => this.songQueue.voiceChannel?.guild;
+  getChannel = () => this.songQueue.voiceChannel;
+  getIsPaused = () => this.songQueue.connection?.dispatcher?.paused;
+  getStatus = () => this.songQueue.connection?.status;
 
   getVoiceStatus = () => ({
-    Queue   : this.getQueue(),
-    Volume  : this.getVolume(),
-    Server  : this.getServer(),
-    Chan    : this.getChannel(),
-    Paused  : this.getIsPaused(),
-    Status  : this.getStatus()
-  })
+    Queue: this.getQueue(),
+    Volume: this.getVolume(),
+    Server: this.getServer(),
+    Chan: this.getChannel(),
+    Paused: this.getIsPaused(),
+    Status: this.getStatus(),
+  });
 
-  Leave = ( io : Server) =>{
-    this.songQueue.connection?.disconnect()
+  Leave = (io: Server) => {
+    this.songQueue.connection?.disconnect();
     this.songQueue.canPlay = false;
-    this.songQueue.voiceChannel= null
-    io.emit("VoiceLeave")
-  }
+    this.songQueue.voiceChannel = null;
+    io.emit("VoiceLeave");
+  };
 
-  Join = ( voiceChan : VoiceChannel , io : Server ) => {
-    if(canJoin(voiceChan)){
-      voiceChan
-        .join()
-        .then(value=>{
-          this.songQueue.voiceChannel = voiceChan
-          this.songQueue.connection = value
-          this.songQueue.canPlay = true
-          io.emit("VoiceJoin")
-        })
+  Join = (voiceChan: VoiceChannel, io: Server) => {
+    if (canJoin(voiceChan)) {
+      voiceChan.join().then((value) => {
+        this.songQueue.voiceChannel = voiceChan;
+        this.songQueue.connection = value;
+        this.songQueue.canPlay = true;
+        io.emit("VoiceJoin");
+      });
     }
-  }
+  };
 
-  Pause = (io : Server) => {
-    this.songQueue.connection?.dispatcher?.pause()
-    io.emit("VoiceChange")
-  }
+  Pause = (io: Server) => {
+    this.songQueue.connection?.dispatcher?.pause();
+    io.emit("VoiceChange");
+  };
 
-  Resume = (io : Server) =>{
-    this.songQueue.connection?.dispatcher?.resume()
-    io.emit("VoiceChange")
-  }
+  Resume = (io: Server) => {
+    this.songQueue.connection?.dispatcher?.resume();
+    io.emit("VoiceChange");
+  };
 
-  Stop = ( io : Server) =>{
-    this.songQueue.queue= [];
+  Stop = (io: Server) => {
+    this.songQueue.queue = [];
     this.songQueue.connection?.dispatcher?.end();
-    io.emit("VoiceChange")
-    return {stopped : true}
-  }
+    io.emit("VoiceChange");
+    return { stopped: true };
+  };
 
-  Skip = ( io : Server) =>{
-    if(this.songQueue.queue.length===0)
-      return {message:"no queue",playing: false}
+  Skip = (io: Server) => {
+    if (this.songQueue.queue.length === 0)
+      return { message: "no queue", playing: false };
     this.songQueue.connection?.dispatcher?.end();
-    io.emit("VoiceChange")
-    return {message:"All is good", playing : true}
-  }
+    io.emit("VoiceChange");
+    return { message: "All is good", playing: true };
+  };
 
-  Play = async ( io : Server, song? : Song, now? : boolean  ) =>{
-    if(!this.songQueue.canPlay) return
-    if(!song)
-      return
-    if(song.type===2)
-      song.title=(await ytdl.getInfo(song.url)).videoDetails.title;
-    if(now){
-      this.songQueue
-        .queue.splice(1,0,song)
-      this.songQueue.connection?.dispatcher?.end()
-    }else
-      this.songQueue
-        .queue.push(song)
-    io.emit("VoiceQueue")
-    if(this.songQueue.queue.length===1){
+  DeleteSong = (io: Server, id: number) => {
+    this.songQueue.queue.splice(id, 1);
+    io.emit("VoiceQueue");
+  };
+
+  Play = async (io: Server, song?: Song, now?: boolean) => {
+    if (!this.songQueue.canPlay || !song) return;
+    if (song.type === 2)
+      song.title = (await ytdl.getInfo(song.url)).videoDetails.title;
+    if (now) {
+      this.songQueue.queue.splice(1, 0, song);
+      this.songQueue.connection?.dispatcher?.end();
+    } else this.songQueue.queue.push(song);
+    io.emit("VoiceQueue");
+    if (this.songQueue.queue.length === 1) {
       this.Player(io);
     }
-  }
-  Player = (io : Server) => {
-    if(this.songQueue.queue.length===0) return
-    if(this.songQueue.canPlay){
+  };
+  Player = (io: Server) => {
+    if (this.songQueue.queue.length === 0) return;
+    if (this.songQueue.canPlay) {
       var dispatch = this.songQueue.connection
-        ?.play((
-          this.songQueue.queue[0].type===SongType.link?
-          this.songQueue.queue[0].url:
-          this.songQueue.queue[0].type===SongType.YouTube?
-          ytdl(this.songQueue.queue[0].url,{filter:'audioonly'}):
-          this.songQueue.queue[0].url
-          ))
-        .on("finish",()=>{
+        ?.play(
+          this.songQueue.queue[0].type === SongType.link
+            ? this.songQueue.queue[0].url
+            : this.songQueue.queue[0].type === SongType.YouTube
+            ? ytdl(this.songQueue.queue[0].url, { filter: "audioonly" })
+            : this.songQueue.queue[0].url
+        )
+        .on("finish", () => {
           this.songQueue.queue.shift();
-          this.Player(io)
-          io.emit("VoiceChange")
+          this.Player(io);
+          io.emit("VoiceChange");
         })
-        .on("error",error=>{
-          io.emit("VoiceError")
-          this.songQueue.playing=false;
-          ErrorLog("VoiceHandler",error.message);
-        })
-        this.songQueue.playing=true;
-        io.emit("VoiceChange")
-        dispatch?.setVolumeLogarithmic(this.songQueue.volume/5)
-      return {message:"Sound started",playing:true}
-    }else{
-      this.songQueue.playing=false;
-      return {message:"Isn't Connected",playing: false}
+        .on("error", (error) => {
+          io.emit("VoiceError");
+          this.songQueue.playing = false;
+          ErrorLog("VoiceHandler", error.message);
+        });
+      this.songQueue.playing = true;
+      io.emit("VoiceChange");
+      dispatch?.setVolumeLogarithmic(this.songQueue.volume / 5);
+      return { message: "Sound started", playing: true };
+    } else {
+      this.songQueue.playing = false;
+      return { message: "Isn't Connected", playing: false };
     }
-  }
+  };
 
-  SetVolume = ( io : Server, volume : number) => {
-    this.songQueue.volume = volume
-    this.songQueue.connection?.dispatcher?.setVolumeLogarithmic(volume/5)
-    io.emit("VoiceChange")
-  }
+  SetVolume = (io: Server, volume: number) => {
+    this.songQueue.volume = volume;
+    this.songQueue.connection?.dispatcher?.setVolumeLogarithmic(volume / 5);
+    io.emit("VoiceChange");
+  };
 }
-
 
 export default VoiceHandler;
