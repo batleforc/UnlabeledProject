9f38aa4741f632cb0459cc4697d0da88e242cd83
Maxime Leriche <maxleriche.60@gmail.com>
2021-04-06 20:02:41 +0200

    [add] Mise en place des nouveaux handler, tray et server dont start a new windows if it still exist

--- public/electron.js
+++ public/electron.js
@@ -1,24 +1,32 @@
 const electron = require("electron");
 const { app, BrowserWindow } = require("electron");
 const isDev = require("electron-is-dev");
+const path = require("path");
 const { startServeur } = require("./elec/server");
 
 let mainWindow;
+const BaseFolder = path.join(__dirname);
+const icon = path.join(BaseFolder, "toolbox.png");
 
-const createWindow = () => {
+const createWindow = ({ showOnLoad = true } = {}) => {
+  if (mainWindow) {
+    mainWindow.focus();
+    return;
+  }
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
     autoHideMenuBar: true,
+    icon: icon,
   });
   mainWindow.loadURL(
     isDev ? "http://localhost:3000" : "http://localhost:5000/"
   ); //`file://${path.join(__dirname, '../build/index.html')}`
-  mainWindow.on("closed", () => (mainWindow = null));
-  startServeur();
+  //mainWindow.on("closed", () => (mainWindow = null));
   mainWindow.once("ready-to-show", () => {
-    mainWindow.show();
+    if (showOnLoad) mainWindow.show();
   });
+  mainWindow.once("closed", () => (mainWindow = null));
   mainWindow.removeMenu();
 };
 
@@ -32,7 +40,17 @@ app.requestSingleInstanceLock() ? init() : app.quit();
 app.on("second-instance", () => mainWindow.show());
 
 app.on("ready", () => {
+  const tray = require("./elec/tray");
+  startServeur();
   createWindow();
+  tray(
+    icon,
+    createWindow,
+    () => {
+      console.log("test");
+    },
+    onQuit
+  );
 });
 
 app.on("window-all-closed", () => {});
