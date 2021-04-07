23a011e6f9ed73db4285e92bddd942380101c93c
Maxime Leriche <maxleriche.60@gmail.com>
2021-04-07 12:49:30 +0200

    [add] Mise en place new logique pour token screen

--- src/Component/Token/TokenItem.tsx
+++ src/Component/Token/TokenItem.tsx
@@ -11,6 +11,7 @@ export const TokenItem = ({
   dispatch,
   value = { label: "Unlabeled Bot" },
   load = false,
+  Bot,
   ...props
 }: any) => {
   var del = () => {
@@ -24,9 +25,9 @@ export const TokenItem = ({
   };
   return (
     <div
-      className={`${
-        load ? "animate-pulse" : ""
-      } mx-1 my-1 border border-primary flex flex-col flex-wrap rounded-md p-1 whitespace-wrap text-center`}
+      className={`${load ? "animate-pulse" : ""} ${
+        value.id === Bot.ActiveBot ? "border-4" : "border"
+      } mx-1 my-1 border-primary flex flex-col flex-wrap rounded-md p-1 whitespace-wrap text-center`}
     >
       <p>{value.label}</p>
       <p>
@@ -51,44 +52,48 @@ export const TokenItem = ({
             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
           />
         </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="bg-gray-400 rounded-md hover:bg-gray-200 mx-0.5"
-          style={{ width: "24px", height: "24px" }}
-          onClick={play}
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="bg-gray-400 rounded-md hover:bg-gray-200 mx-0.5"
-          style={{ width: "24px", height: "24px" }}
-          onClick={stop}
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-          />
-        </svg>
+        {value.id !== Bot.ActiveBot && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="bg-gray-400 rounded-md hover:bg-gray-200 mx-0.5"
+            style={{ width: "24px", height: "24px" }}
+            onClick={play}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        )}
+        {value.id === Bot.ActiveBot && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="bg-gray-400 rounded-md hover:bg-gray-200 mx-0.5"
+            style={{ width: "24px", height: "24px" }}
+            onClick={stop}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+            />
+          </svg>
+        )}
       </div>
     </div>
   );
--- src/Component/Token/index.tsx
+++ src/Component/Token/index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import TokenItem from "./TokenItem";
 import { showTokenForm } from "../../Action/showModal";
+import { Link } from "react-router-dom";
 
-export const index = ({ Token, dispatch, ...props }: any) => {
+export const index = ({ Token, dispatch, Bot, ...props }: any) => {
   return (
     <div className="px-2">
       {Token.Pending && <div></div>}
@@ -26,6 +27,34 @@ export const index = ({ Token, dispatch, ...props }: any) => {
         value="Add Bot"
         onClick={() => dispatch(showTokenForm())}
       />
+      {Bot.ActiveBot !== -1 && (
+        <div className="flex flex-col">
+          <h1>Bot actif :</h1>
+          <div className="flex m-2">
+            <img
+              alt="Icon du bot"
+              className="w-24 h-24 rounded"
+              src={Bot.user.avatarURL}
+            />
+            <div className="p-2 flex-1">
+              <p>usename : {Bot.user.username}</p>
+              <p>tag : {Bot.user.tag} </p>
+            </div>
+            <div className="justify-self-end flex-2">
+              <h2>Guild disponible:</h2>
+              {Bot.Serveur.map((value: any) => (
+                <div key={value.id}> =&gt;{value.ServeurName}</div>
+              ))}
+            </div>
+          </div>
+          <Link
+            className="hover:bg-gray-200 mx-0.5 m-2 rounded-md self-center"
+            to="/voice"
+          >
+            Vers la SoundBoard
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
