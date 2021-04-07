0e3880a6c3b1fa1754c2c39d0032cf34cd260436
Maxime Leriche <maxleriche.60@gmail.com>
2021-04-07 16:47:29 +0200

    [add] retrait du message pas de token devenue innutile

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -8,7 +8,7 @@ import {
   CanPlay,
   BotServeurChanGetter,
 } from "../Action/Bot";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import TokenComponent from "./Token";
 import NavBar from "./navbar";
 import BotCanPlay from "./Bot/modal";
@@ -56,9 +56,6 @@ export const App = ({ dispatch, Token, Event, Bot }: any) => {
   return (
     <div className="App">
       <NavBar />
-      {!Token.Pending && Token.AllToken.length === 0 && (
-        <Link to="/token">Est mec ta pas de Bot ! clic moi dessus</Link>
-      )}
       <Route path={["/","/token"]} exact component={TokenComponent} />
       <Route path="/voice" component={Voice} />
       <BotCanPlay />
