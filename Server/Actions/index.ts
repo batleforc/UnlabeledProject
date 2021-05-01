60d6521519354d2dfb4954f3477e3f6c10202627
Maxime Leriche <maxleriche.60@gmail.com>
2021-05-01 21:34:33 +0200

    [add] include des store event token et voicehandler

--- Server/Actions/index.ts
+++ Server/Actions/index.ts
@@ -1,9 +1,15 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import Board from "./Board";
+import Event from './Event';
+import Token from './Token';
+import VoiceHandler from './VoiceHandler';
 
 const store = configureStore({
   reducer: {
     Board,
+    Event,
+    Token,
+    VoiceHandler
   },
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware(),
