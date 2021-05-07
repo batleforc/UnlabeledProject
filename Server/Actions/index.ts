c31d41b2609ee3530b758c9e5a7c8b11951c5e67
Maxime Leriche <maxleriche.60@gmail.com>
2021-05-07 17:46:59 +0200

    [add] Ajout de voicehandler

--- Server/Actions/index.ts
+++ Server/Actions/index.ts
@@ -1,19 +1,20 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import Board from "./Board";
-import Event from './Event';
-import Token from './Token';
-import VoiceHandler from './VoiceHandler';
+import Event from "./Event";
+import Token from "./Token";
+import Bot from "./Bot";
+import VoiceHandler from "./VoiceHandler";
 
 const store = configureStore({
   reducer: {
     Board,
     Event,
     Token,
-    VoiceHandler
+    VoiceHandler,
+    Bot,
   },
-  middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware(),
-  devTools:true
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: true,
 });
 
 export default store;
