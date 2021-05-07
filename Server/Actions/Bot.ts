0eeb7e4c9dad6dec15faebb800cb36e7c1842dea
Maxime Leriche <maxleriche.60@gmail.com>
2021-05-07 19:36:01 +0200

    [add] crete BotGetUSer and BotLogin

--- Server/Actions/Bot.ts
+++ Server/Actions/Bot.ts
@@ -1,21 +1,44 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { DiscordClient } from "../index";
+import { DiscordClient, Db } from "../index";
+
+export const BotGet = createAsyncThunk("Bot/Get", async () =>
+  DiscordClient.GetUser()
+);
+
+export const BotLogin = createAsyncThunk(
+  "Bot/Login",
+  async ({ id }: { id: string }) => {
+    return Db.GetToken(id).then((value) => {
+      DiscordClient.LoginClient(value.token, Number(id));
+      return Number(id);
+    });
+  }
+);
 
 export interface iBot {
   Ready: Boolean;
   BotId: number;
-  BotStatut: string;
+  Presence: {
+    Status: string;
+
+  }
 }
 const initialState = {
   Ready: false,
   BotId: -1,
-  BotStatut:""
+  Presence: {
+    Status : ""
+  }
 } as iBot;
 
 const Bot = createSlice({
   name: "Bot",
   initialState,
   reducers: {},
+  extraReducers: (builder) =>
+    builder.addCase(BotLogin.fulfilled, (state, { payload }) => {
+      state.BotId = payload;
+    }),
 });
 
 export default Bot.reducer;
