0ecc3df74c40448719a0d3b0c44b34f330db81d0
Maxime Leriche <maxleriche.60@gmail.com>
2021-05-09 18:01:40 +0200

    [add] setactivity et getactivity

--- Server/Actions/Bot.ts
+++ Server/Actions/Bot.ts
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { ActivityType } from "discord.js";
 import { DiscordClient, Db } from "../index";
 
 export const BotGet = createAsyncThunk("Bot/Get", async () =>
@@ -15,20 +16,44 @@ export const BotLogin = createAsyncThunk(
   }
 );
 
+export const BotSetActivity = createAsyncThunk(
+  "Bot/SetActivity",
+  async (
+    {
+      online,
+      name,
+      type,
+    }: { online: boolean; name: string; type: ActivityType },
+    { dispatch }
+  ) => {
+    DiscordClient.SetPresence(online, name, type);
+    dispatch(BotGetActivity());
+  }
+);
+
+export const BotGetActivity = createAsyncThunk("Bot/GetActivity", async () => ({
+  status: DiscordClient.GetPresence().status,
+  name: DiscordClient.GetPresence().activities[0].name,
+  type: DiscordClient.GetPresence().activities[0].type,
+}));
+
 export interface iBot {
   Ready: Boolean;
   BotId: number;
   Presence: {
     Status: string;
-
-  }
+    name: string;
+    type: string;
+  };
 }
 const initialState = {
   Ready: false,
   BotId: -1,
   Presence: {
-    Status : ""
-  }
+    Status: "",
+    name: "",
+    type: "",
+  },
 } as iBot;
 
 const Bot = createSlice({
@@ -36,9 +61,15 @@ const Bot = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) =>
-    builder.addCase(BotLogin.fulfilled, (state, { payload }) => {
-      state.BotId = payload;
-    }),
+    builder
+      .addCase(BotLogin.fulfilled, (state, { payload }) => {
+        state.BotId = payload;
+      })
+      .addCase(BotGetActivity.fulfilled, (state, { payload }) => {
+        state.Presence.Status = payload.status;
+        state.Presence.name = payload.name;
+        state.Presence.type = payload.type;
+      }),
 });
 
 export default Bot.reducer;
