dd1d373988685b82b57f958ddacb793a45ef8c15
Maxime Leriche <maxleriche.60@gmail.com>
2021-05-11 23:41:21 +0200

    [add] Intégration du store Token dans l'api

--- Server/Router/Api/Token.ts
+++ Server/Router/Api/Token.ts
@@ -1,37 +1,52 @@
-import Router from '@koa/router'
+import Router from "@koa/router";
+import { EnhancedStore } from "@reduxjs/toolkit";
+import { TokenCreate, TokenDelete, TokenUpdate } from "../../Actions/Token";
 var Token = new Router();
 
-Token
-  .get("/", async (ctx : any, next :any) => {
-    ctx.body = await ctx.Db.GetAllToken()
-    await next()
-  })
-  .post("/", async (ctx : any , next : any) => {
+Token.get("/", async (ctx: any, next: any) => {
+  ctx.body = (ctx.Redux as EnhancedStore).getState().Token.Token;
+  await next();
+})
+  .post("/", async (ctx: any, next: any) => {
     var body = ctx.request.body;
-    if(body.label===undefined||body.token===undefined){
-      ctx.status = 400
-      ctx.body={message:"Error one argument missing"}
-    }else
-      ctx.body = ctx.Db.InsertToken(body.label,body.token)
-    await next()
+    if (body.label === undefined || body.token === undefined) {
+      ctx.status = 400;
+      ctx.body = { message: "Error one argument missing" };
+    } else {
+      await ctx.Redux.dispatch(
+        TokenCreate({ label: body.label, token: body.token })
+      );
+      ctx.body = (ctx.Redux as EnhancedStore).getState().Token.Token;
+    }
+    await next();
   })
-  .delete("/:id", async (ctx : any , next : any) => {
+  .delete("/:id", async (ctx: any, next: any) => {
     var params = ctx.params.id;
-    if(params===undefined){
-      ctx.status = 400
-      ctx.body={message:"Error one argument missing"}
-    }else
-      ctx.body = ctx.Db.DeleteToken(params)
-    await next()
+    if (params === undefined) {
+      ctx.status = 400;
+      ctx.body = { message: "Error one argument missing" };
+    } else {
+      await ctx.Redux.dispatch(TokenDelete({ TokenId: Number(params) }));
+    }
+    ctx.body = (ctx.Redux as EnhancedStore).getState().Token.Token;
+    await next();
   })
-  .put("/:id", async (ctx : any , next : any) => {
+  .put("/:id", async (ctx: any, next: any) => {
     var body = ctx.request.body;
-    if(ctx.params.id===undefined||body.label===undefined||body.token===undefined){
-      ctx.status = 400
-      ctx.body={message:"Error one argument missing"}
-    }else
-      ctx.body = ctx.Db.EditToken(ctx.params.id,body.label,body.token)
-    await next()
-  })
+    if (
+      ctx.params.id === undefined ||
+      body.label === undefined ||
+      body.token === undefined
+    ) {
+      ctx.status = 400;
+      ctx.body = { message: "Error one argument missing" };
+    } else {
+      await ctx.Redux.dispatch(
+        TokenUpdate({ id: ctx.params.id, label: body.label, token: body.token })
+      );
+    }
+    ctx.body = (ctx.Redux as EnhancedStore).getState().Token.Token;
+    await next();
+  });
 
 export default Token;
