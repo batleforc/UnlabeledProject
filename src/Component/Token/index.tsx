6710910c466a008d8012c6c666fb4e543cb2ce62
Maxime Leriche <maxleriche.60@gmail.com>
2021-05-17 01:00:38 +0200

    [add] ajout de la partie gestion de la présence

--- src/Component/Token/index.tsx
+++ src/Component/Token/index.tsx
@@ -4,6 +4,13 @@ import TokenItem from "./TokenItem";
 import { showTokenForm } from "../../Action/showModal";
 import { Link } from "react-router-dom";
 import TokenModalForm from "./TokenModalForm";
+import {
+  BotPresenceGetter,
+  BotPresenceSetter,
+  setName,
+  setStatus,
+  setType,
+} from "../../Action/Bot";
 
 export const index = ({ Token, dispatch, Bot, ...props }: any) => {
   return (
@@ -41,11 +48,84 @@ export const index = ({ Token, dispatch, Bot, ...props }: any) => {
               <p>Username : {Bot.user.username}</p>
               <p>Tag : {Bot.user.tag} </p>
             </div>
+            <div className="p-2 flex-1">
+              <label>
+                <select
+                  value={Bot.Presence.type}
+                  onChange={(e) => dispatch(setType(e.currentTarget.value))}
+                  className="py-1 my-3 text-center rounded-md border border-primary px-2"
+                >
+                  <option key="none" value="">
+                    Type de presence
+                  </option>
+                  {[
+                    "PLAYING",
+                    "STREAMING",
+                    "LISTENING",
+                    "WATCHING",
+                    "CUSTOM_STATUS",
+                    "COMPETING",
+                  ].map((value) => (
+                    <option key={value} value={value}>
+                      {value}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label className="text-center flex">
+                <span>Presence : </span>
+                <input
+                  className="px-1 rounded-md border border-primary block"
+                  placeholder="Presence"
+                  type="text"
+                  name="Presence"
+                  value={Bot.Presence.name}
+                  onChange={(e) => dispatch(setName(e.currentTarget.value))}
+                />
+              </label>
+              <label className="text-center flex">
+                <select
+                  value={Bot.Presence.Status}
+                  onChange={(e) => dispatch(setStatus(e.currentTarget.value))}
+                  className="py-1 my-3 text-center rounded-md border border-primary px-2"
+                >
+                  <option key="none" value="">
+                    Type de Status
+                  </option>
+                  {["online", "idle", "dnd", "invisible"].map((value) => (
+                    <option key={value} value={value}>
+                      {value}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <button
+                className="py-1 my-3 text-center rounded-md border border-primary px-2"
+                onClick={() =>
+                  dispatch(
+                    BotPresenceSetter({
+                      online: Bot.Presence.Status,
+                      name: Bot.Presence.name,
+                      type: Bot.Presence.type,
+                    })
+                  )
+                }
+              >
+                Appliquer
+              </button>
+              <button
+                className="py-1 my-3 text-center rounded-md border border-primary px-2"
+                onClick={() => dispatch(BotPresenceGetter())}
+              >
+                Reset
+              </button>
+            </div>
             <div className="justify-self-end flex-2">
               <h2>Guild disponible:</h2>
-              {Array.isArray(Bot.Serveur)&&Bot.Serveur.map((value: any) => (
-                <div key={value.id}> =&gt;{value.ServeurName}</div>
-              ))}
+              {Array.isArray(Bot.Serveur) &&
+                Bot.Serveur.map((value: any) => (
+                  <div key={value.id}> =&gt;{value.ServeurName}</div>
+                ))}
             </div>
           </div>
           <Link
